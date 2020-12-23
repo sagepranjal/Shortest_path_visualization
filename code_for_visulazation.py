cbc59bf6e99e667d75bff35d35490165d1554afe
sagepranjal <pranjalthakur98@gmail.com>
2020-12-23 14:28:25 +0530

    Update code_for_visulazation.py

--- code_for_visulazation.py
+++ code_for_visulazation.py
@@ -14,7 +14,9 @@ pygame.display.set_caption("A* path finding algorithm")
 
 #colors for the points
 
-RED = (255, 0, 0)
+# RED = (255, 0, 0)
+
+RED = (144,238,144)
 GREEN = (0, 255, 0)
 BLUE = (0, 255, 0)
 YELLOW = (255, 255, 0)
